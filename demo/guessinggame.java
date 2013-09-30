83725f1838c6959b180a6f34df4209e537ddf2e2
bdpelc <bdpelc@gmail.com>
2013-09-30 16:27:43 -0500

    Modified program to also print the number of attempts when the user guessed it.

--- demo/guessinggame.java
+++ demo/guessinggame.java
@@ -12,6 +12,8 @@ public class guessinggame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// Variable to store number of guesses
+		int guesses = 0;
 		// Random Number Generator
 		int random = 0;
 		Random rg = new Random();
@@ -21,14 +23,21 @@ public class guessinggame {
 		// Read user input
 		System.out.println("what is the random number?");
 		int user = input.nextInt();
+		guesses ++;
 		// Compare user input to the random number
 		while(user!=random){
-			if (user > random)
-				System.out.println("Too Hight!");
-			else if (user < random)
-				System.out.println("Too Low!");
+			if (user > random){
+				System.out.println("Too Hight! Try Again");
+				int user = input.nextInt();
+				guesses ++;
+			}
+			else if (user < random){
+				System.out.println("Too Low! Try Again");
+				int user = input.nextInt();
+				guesses ++;
+			}
 		}
-		System.out.println("You Got It!");
+		System.out.println("You Got It!  Number of guesses: "+guesses);
 		
 	} // End Main
 
